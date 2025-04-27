de23b76aa95afbae3d986188c0edb5d3bae83f2f
danah-frontend <darkmount29@gmail.com>
2025-04-27 21:39:27 +0300

    test of Calculator

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -3,29 +3,34 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
-
+    Calculator calculator = new Calculator();
     @Test
     void add() {
         //TODO inser your realisation in test-method add
+        assertEquals(4,calculator.add(2,2));
     }
 
     @Test
     void dif() {
         //TODO inser your realisation in test-method dif
+        assertEquals(4,calculator.dif(8,4));
     }
 
     @Test
     void div() {
         //TODO inser your realisation in test-method div
+        assertEquals(1,calculator.div(2,2));
     }
 
     @Test
     void times() {
         //TODO inser your realisation in test-method times
+        assertEquals(4,calculator.times(2,2));
     }
 
     @Test
     void solver() {
         //TODO inser your realisation in test-method solver
+        assertEquals(30,calculator.solver(4,7,2,3));
     }
 }
